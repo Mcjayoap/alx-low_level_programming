cfb440822fda76fbad64a85ae8d7888a05303a0d
Mcjayoap <drdonjoe@gmail.com>
2022-09-09 03:52:20 -0700

    commit this

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main-entry point for the program.
- * 
- *
- * Return:0 if no errors, return non zero if errors
- */
+* main-entry point for the program.
+* 
+* Return:0 if no errors, return non zero if errors
+*/
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
