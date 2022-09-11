23a78a7d8511a054f72f5996683b8b17c829220a
Mcjayoap <drdonjoe@gmail.com>
2022-09-11 11:02:43 +0100

    home

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -18,6 +18,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	
 	return (0);
 }
 
