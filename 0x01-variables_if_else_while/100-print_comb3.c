657f4d751d32dd878568a19c207bccb76e782883
Mcjayoap <drdonjoe@gmail.com>
2022-09-12 17:53:42 -0700

    so

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	int d = 48, a = 48;
 	while (d < 58)
+
 	{
 		a = 48;
 		while (a < 58)
@@ -17,7 +18,6 @@ int main(void)
 			{
 				putchar(d);
 				putchar(a);
-				
 				if (!(d == 56 && a == 57))
 				{
 					putchar(',');
