06f661df94b2f8a6c408ef2cc4ba6345f5670c63
Mcjayoap <drdonjoe@gmail.com>
2022-09-13 21:55:34 -0700

    go

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * test_isalpha - Test the _isalpha function
+ *
+ * @n: Number to pass to _isalpha function
+ * */
+void test_isalpha(int n)
+{
+	int r;
+	r = _isalpha(n);
+	_putchar('\n');
+}
