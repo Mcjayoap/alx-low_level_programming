8b5b6472aaf2b3e3c638fe609f9211173b50ce38
Mcjayoap <drdonjoe@gmail.com>
2022-09-14 20:25:49 -0700

    go

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -32,9 +32,9 @@ void times_table(void)
 			else if ((product < 10) && (y != 0))
 			{
 				_putchar(' ');
-				_putchar((product % 10) + '0')
+				_putchar((product % 10) + '0');
 			}
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
