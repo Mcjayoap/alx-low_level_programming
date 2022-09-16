cb64ba15cfb774f00e531894ca73220d6eb855ec
Mcjayoap <drdonjoe@gmail.com>
2022-09-15 19:35:27 -0700

    learning debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -8,9 +9,9 @@
 int main(void)
 {
 	int i;
-
-	i = 98;
-	positive_or_negative(i);
+	
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
 
 	return (0);
 }
