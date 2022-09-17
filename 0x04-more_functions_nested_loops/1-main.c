893f86859c1cd6331cbfa9c5d7ca33dbd0d85d5c
Mcjayoap <drdonjoe@gmail.com>
2022-09-16 19:41:40 -0700

    ok now

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -12,8 +12,8 @@ int main(void)
 	char c;
 
 	c = '0';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
