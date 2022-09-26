4b4dad48237057d1a85b9f66ad8edd6efdf3c715
Mcjayoap <drdonjoe@gmail.com>
2022-09-26 16:02:14 -0700

    correct

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * searches a string for any of a set of bytes.
+ * _strpbrk - searches a string for any of a set of bytes.
  * @s: string
  * @accept: string
  * Return: pointer to the byte in s.
