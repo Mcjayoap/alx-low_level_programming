cb01136ba04269420004d17d912024b29feb8e95
Mcjayoap <drdonjoe@gmail.com>
2022-09-26 13:18:26 -0700

    good to go

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,18 +2,18 @@
 /**
  * _strchr - locates a character in a string.
  * @s: string
- * @b: character to locate
+ * @c: character to locate
  * Return: ppointer to;the first occurence of the character
  */
 char *_strchr(char *s, char c)
 {
 	while (*s != 0)
 	{
-		if (*s == b)
+		if (*s == c)
 			return (s);
 		s++;
 	}
-	if (*s == b)
+	if (*s == c)
 		return (s);
 
 	return ('\0');
