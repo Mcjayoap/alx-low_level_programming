04299de5f0e1bb22b737dd45b14f5b5cca5d9130
Mcjayoap <drdonjoe@gmail.com>
2022-09-29 21:20:36 -0700

    .

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-				return (1);
+			return (1);
 	}
 	return (0);
 }
