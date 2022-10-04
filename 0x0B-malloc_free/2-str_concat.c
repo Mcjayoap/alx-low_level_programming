67126cb1d77f03dda6429abdee1880d2744faa30
Mcjayoap <drdonjoe@gmail.com>
2022-10-04 06:47:16 -0700

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int init, concat_init = 0, len = 0;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 
 	for (init = 0; s1[init] || s2[init]; init++)
 		len++;
