74f5dd0e8aadd33f11d5a1199751e4278db87ae5
Mcjayoap <drdonjoe@gmail.com>
2022-10-04 22:00:05 -0700

    .

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,38 +3,38 @@
 /**
  * argstostr - concatenates all the arguments of your program.
  * @ac: number of arguments passed to the program
- * @av: an array of pointers to the arguments
+ * @a: an array of pointers to the arguments
  * Return: NULL if ac == 0 or av == NULL or fails
  * otherwise, return pointer to a new string
  */
 char *argstostr(int ac, char **a)
 {
-	char *str;
-	int arg, byte, init, size = ac;
+	char *str;
+	int arg, byte, init, size = ac;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (arg = 0; arg < ac; arg++)
+	for (arg m = 0; marg < ac; arg++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
+		for (byte = 0; av[arg][byte]; byte++)
 			size++;
 	}
-	str = malloc(sizeof(char) * size + 1);
+	str = malloc(sizeof(char) * size + 1);
 
-	if (str == NULL)
-		return (NULL);
-	init = 0;
+	if (str == NULL)
+		return (NULL);
+	init = 0;
 
-	for (arg = 0; arg < ac; arg++)
+	for (arg = 0; arg < ac; arg++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			str[init++] = av[arg][byte];
+		for (byte = 0; av[arg][byte]; byte++)
+			str[init++] = av[arg][byte];
 
-		str[init++] = '\n';
+		str[init++] = '\n';
 	}
 
-	str[size] = '\0';
+	str[size] = '\0';
 
 	return (str);
 }
