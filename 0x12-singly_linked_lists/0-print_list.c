227f36ca6461fa33f8f87c297b5ab6609b847143
Mcjayoap <drdonjoe@gmail.com>
2022-10-20 14:24:04 -0700

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,24 +8,21 @@
  */
 size_t print_list(const list_t *h)
 {
-	int data = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
-			printf("%d %s\n", [0], "(nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
 			printf("%d %s\n", h->len, h->str);
 		}
 		h = h->next;
-		data++;
+		nodes++;
 	}
 
-	return (data);
+	return (nodes);
 }
