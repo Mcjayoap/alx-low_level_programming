a89e800cf3f41c3304a6ff49fad78099c1875513
Mcjayoap <drdonjoe@gmail.com>
2022-10-21 01:30:54 -0700

    add node at the beginning of List

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,40 +10,29 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *newNode;
+	int c;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	for (c = 0; c != '\0'; c++);
 
-	dup = strdup(str);
-	if (str == NULL)
+	newNode = malloc(sizeof(struct list_s));
+	if (newNode == NULL)
 	{
-		free(new);
+		free(newNode);
+		return (NULL);
+	}
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
 		return (NULL);
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	newNode->len = c;
+	newNode->next = *head;
 
-	new->str = dup;
-	new->len = len;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	else
-	{
-		new = *head;
-		while (new->next != NULL)
-			new = new->next;
-		new->next = NULL;
-	}
-
+	*head = newNode;
 	return (*head);
 }
