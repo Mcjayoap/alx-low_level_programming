93a2e357d1f2c4bb77d5d1b8b3d1ba4aa413f1d2
Mcjayoap <drdonjoe@gmail.com>
2022-11-01 22:54:03 -0700

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	op = open(filemaster, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(op, taste_content, len);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -2)
 		return (-1);
 
-	close(o);
+	close(op);
 	return (1);
 }
