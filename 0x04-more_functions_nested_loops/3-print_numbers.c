9409200c46c2127225a7b13d73d090c0ec8cf45a
Mcjayoap <drdonjoe@gmail.com>
2022-11-19 12:50:59 +0000

    correcting 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+	return;
+}
