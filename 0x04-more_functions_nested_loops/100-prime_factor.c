d68c15dc4a8f7bdb8ec4e90fcd20ec4670dca38f
Mcjayoap <drdonjoe@gmail.com>
2022-11-24 15:30:59 +0000

    print highest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,13 +11,13 @@ void print_primeFactor(long int n)
 {
 	int maxFactor;
 
-	for (maxFactor = (int) sqrt (n); maxFactor > 2; maxFactor++)
+	for (maxFactor = (int) sqrt(n); maxFactor > 2; maxFactor++)
 	{
 		if (n % maxFactor == 0)
-			{
-				printf("%d if the highest prime factor of the number %ld\n", maxFactor, n);
-				break;
-			}
+		{
+			printf("%d if the highest prime factor of the number %ld\n", maxFactor, n);
+			break;
+		}
 	}
 }
 
