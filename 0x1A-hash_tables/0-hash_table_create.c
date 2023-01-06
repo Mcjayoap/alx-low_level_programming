40cf97f872aff2e6512b05beefa7add3f9675f3b
Mcjayoap <mcjayoap>
2023-01-06 12:32:11 +0000

    task 0 done,check

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of the array.
+ * Return: pointer to the newly created hash table
+ *	otherwise, return NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t*) *size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
